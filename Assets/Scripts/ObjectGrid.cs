b7dc473769cd3c339b3eb254564869a14c083d10
moog2001 <moogforhire@gmail.com>
2021-08-02 00:03:20 +0800

    edited the visible player cube

--- Assets/Scripts/ObjectGrid.cs
+++ Assets/Scripts/ObjectGrid.cs
@@ -9,7 +9,8 @@ public class ObjectGrid : MonoBehaviour
     Transform objectTransform;
     Collider playerCollider;
     Vector3 spawnPoint;
-
+    float xValue;
+    Vector3 testValue;
     public enum Orientations
     {
         Right, Left, Top, Forward, Back
@@ -20,44 +21,54 @@ public class ObjectGrid : MonoBehaviour
     {
         player = GameObject.Find("PlayerVisible");
         playerCollider = player.GetComponent<Collider>();
-
-        switch (orientation)
-        {
-            case Orientations.Forward:
+        /*
+                switch (orientation)
                 {
-                    spawnPoint = player.transform.InverseTransformPoint(new Vector3(playerCollider.bounds.center.x, playerCollider.bounds.center.y, playerCollider.bounds.max.z));
-                    break;
+                    case Orientations.Forward:
+                        {
+                            spawnPoint = player.transform.InverseTransformPoint(new Vector3(playerCollider.bounds.center.x, playerCollider.bounds.center.y, playerCollider.bounds.max.z));
+                            break;
+                        }
+                    case Orientations.Back:
+                        {
+
+                            spawnPoint = player.transform.InverseTransformPoint(new Vector3(playerCollider.bounds.center.x, playerCollider.bounds.center.y, playerCollider.bounds.min.z));
+                            break;
+                        }
+                    case Orientations.Left:
+                        {
+
+                            spawnPoint = player.transform.InverseTransformPoint(new Vector3(playerCollider.bounds.min.x, playerCollider.bounds.center.y, playerCollider.bounds.center.z));
+                            break;
+                        }
+
+                    case Orientations.Right:
+                        {
+
+                            spawnPoint = player.transform.InverseTransformPoint(new Vector3(playerCollider.bounds.max.x, playerCollider.bounds.center.y, playerCollider.bounds.center.z));
+                            break;
+                        }
+                    case Orientations.Top:
+                        {
+
+                            spawnPoint = player.transform.InverseTransformPoint(new Vector3(playerCollider.bounds.center.x, playerCollider.bounds.max.y, playerCollider.bounds.center.z));
+                            break;
+                        }
                 }
-            case Orientations.Back:
-                {
 
-                    spawnPoint = player.transform.InverseTransformPoint(new Vector3(playerCollider.bounds.center.x, playerCollider.bounds.center.y, playerCollider.bounds.min.z));
-                    break;
-                }
-            case Orientations.Left:
-                {
+                gameObject.transform.SetParent(player.transform, false);
+                gameObject.transform.localPosition = spawnPoint;
+                */
+        print(playerCollider.bounds.max.x);
+        print(playerCollider.bounds.min.x);
+        testValue = new Vector3(playerCollider.bounds.max.x, playerCollider.bounds.min.y, playerCollider.bounds.max.z) - new Vector3(playerCollider.bounds.min.x, playerCollider.bounds.min.y, playerCollider.bounds.min.z);
+        print(testValue);
+        xValue = testValue.x;
+        print(xValue);
 
-                    spawnPoint = player.transform.InverseTransformPoint(new Vector3(playerCollider.bounds.min.x, playerCollider.bounds.center.y, playerCollider.bounds.center.z));
-                    break;
-                }
-
-            case Orientations.Right:
-                {
-
-                    spawnPoint = player.transform.InverseTransformPoint(new Vector3(playerCollider.bounds.max.x, playerCollider.bounds.center.y, playerCollider.bounds.center.z));
-                    break;
-                }
-            case Orientations.Top:
-                {
-
-                    spawnPoint = player.transform.InverseTransformPoint(new Vector3(playerCollider.bounds.center.x, playerCollider.bounds.max.y, playerCollider.bounds.center.z));
-                    break;
-                }
-        }
-
-        gameObject.transform.SetParent(player.transform, false);
-        gameObject.transform.localPosition = spawnPoint;
     }
+
+
 
     // Update is called once per frame
     void Update()
