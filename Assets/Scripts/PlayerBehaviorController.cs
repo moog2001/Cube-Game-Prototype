9e5ffc92515889bca422a2359b3eb2180c56f8e4
moog2001 <moogforhire@gmail.com>
2022-02-10 20:25:29 +0800

    player child object rotation fix

--- Assets/Scripts/PlayerBehaviorController.cs
+++ Assets/Scripts/PlayerBehaviorController.cs
@@ -28,17 +28,24 @@ public class PlayerBehaviorController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E) && !test)
         {
-            SpawnPowerUp(playerGridCreation.gridCenterTransforms[0][0, 0].position, prefab);
-            SpawnPowerUp(playerGridCreation.gridCenterTransforms[1][0, 0].position, prefab);
-            SpawnPowerUp(playerGridCreation.gridCenterTransforms[2][0, 0].position, prefab);
-            SpawnPowerUp(playerGridCreation.gridCenterTransforms[3][0, 0].position, prefab);
-            SpawnPowerUp(playerGridCreation.gridCenterTransforms[4][0, 0].position, prefab);
+            for (int i = 0; i < playerGridCreation.gridCenterTransforms.GetLength(0); i++)
+            {
+                for (int j = 0; j < playerGridCreation.gridCenterTransforms[i].GetLength(0); j++)
+                {
+                    for (int n = 0; n < playerGridCreation.gridCenterTransforms[i].GetLength(1); n++)
+                    {
+                        SpawnPowerUp(playerGridCreation.gridCenterTransforms[i][j, n].position, prefab);
+                        Debug.DrawRay(transform.TransformPoint(playerGridCreation.gridCenterTransforms[i][j, n].position*1.2f), transform.position - transform.TransformPoint(playerGridCreation.gridCenterTransforms[i][j, n].position*1.2f));
+                    }
+                }
+            }
+
         }
 
     }
     void SpawnPowerUp(Vector3 position, GameObject prefab)
     {
-        Vector3 rayStartPosition = position * 1.1f;
+        Vector3 rayStartPosition = position * 1.001f;
         RaycastHit hit;
         Physics.Raycast(transform.TransformPoint(rayStartPosition), transform.position - transform.TransformPoint(rayStartPosition), out hit, 0.2f);
         GameObject powerUpObject = Instantiate(prefab, transform.TransformPoint(position), prefab.transform.rotation);
